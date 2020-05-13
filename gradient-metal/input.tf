17364bdde96f517d0f896e00647ebdda6c8fa698
Philip Reichenberger <hi@philipreichenberger.com>
2020-05-12 20:42:36 -0400

    Add cpu_selector and gpu_selector

--- gradient-metal/input.tf
+++ gradient-metal/input.tf
@@ -28,10 +28,13 @@ variable "ssh_user" {
     default = "ubuntu"
 }
 
-
-variable "global_selector" {
-    description = "Node selector prefix used globally"
-    default = ""
+variable "cpu_selector" {
+    description = "Node CPU selector"
+    default = "metal-cpu"
+}
+variable "gpu_selector" {
+    description = "Node GPU selector"
+    default = "metal-gpu"
 }
 
 variable "local_storage_path" {
