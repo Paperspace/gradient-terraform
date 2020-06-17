a2094f124d592ed8c2745634cef20cbf34505eff
Jared Scheib <jared.scheib@gmail.com>
2020-06-17 12:16:51 -0400

    try no sleep after reboot to see if connection still doesn't work
    this would be more ideal.

--- gradient-metal/modules/kubernetes/main.tf
+++ gradient-metal/modules/kubernetes/main.tf
@@ -76,14 +76,10 @@ resource "null_resource" "rke_nodes_wait" {
     }
 
     provisioner "local-exec" {
-        command = "sleep 10 && echo 'Waiting for ${local.rke_nodes[count.index].ip}'"
+        command = "echo 'Waiting for ${local.rke_nodes[count.index].ip}'"
     }
 
     provisioner "remote-exec" {
-        inline = [
-            "echo pineapple"
-        ]
-
         connection {
             type     = "ssh"
             user     = var.ssh_user
