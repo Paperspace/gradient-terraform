d4465327bfdcb89fdfc343db0321a7807ba2529f
Jared Scheib <jared.scheib@gmail.com>
2020-07-02 15:31:29 -0400

    update name of aws bucket for managed ps cloud

--- gradient-ps-cloud/backends/managed.tf
+++ gradient-ps-cloud/backends/managed.tf
@@ -1,6 +1,6 @@
 terraform {
     backend "s3" {
-        bucket = "managed-gradient-ps-cloud"
+        bucket = "gradient-managed-ps-cloud"
         key    = "gradient-processing-ps-cloud-managed"
         region = "us-east-1"
     }
