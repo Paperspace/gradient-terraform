577986931c3e4c1429325f67b9c431f543a2acba
Philip Reichenberger <philip@paperspace.com>
2020-07-03 14:22:20 -0400

    remove secret storage

--- gradient-ps-cloud/main.tf
+++ gradient-ps-cloud/main.tf
@@ -209,8 +209,6 @@ resource "null_resource" "complete_cluster_create" {
 
     provisioner "local-exec" {
         command = <<EOF
-            curl -H 'Content-Type:application/json' -H 'X-API-Key: ${var.cluster_apikey}' -XPUT '${var.api_host}/clusters/secrets/${var.cluster_handle}_ssh_key_base64' -d '{"clusterId":"${var.cluster_handle}", "value":"${base64encode(tls_private_key.ssh_key.private_key_pem)}"}'
-            curl -H 'Content-Type:application/json' -H 'X-API-Key: ${var.cluster_apikey}' -XPUT '${var.api_host}/clusters/secrets/${var.cluster_handle}_kubeconfig' -d '{"clusterId":"${var.cluster_handle}","value":"${base64encode(file(pathexpand(var.kubeconfig_path)))}"}'
             curl -H 'Content-Type:application/json' -H 'X-API-Key: ${var.cluster_apikey}' -XPOST '${var.api_host}/clusters/updateCluster' -d '{"id":"${var.cluster_handle}", "attributes":{"networkId":"${paperspace_network.network.id}"}}'
         EOF
     }
