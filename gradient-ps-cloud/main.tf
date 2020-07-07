f193255ea6024719bf654bd9a76f4a365e7c9eee
Philip Reichenberger <philip@paperspace.com>
2020-07-07 12:08:32 -0400

    add ssh connection check

--- gradient-ps-cloud/main.tf
+++ gradient-ps-cloud/main.tf
@@ -66,6 +66,15 @@ resource "paperspace_machine" "gradient_main" {
     network_id = paperspace_network.network.handle
     live_forever = true
 
+    provisioner "remote-exec" {
+        connection {
+            type     = "ssh"
+            user     = "paperspace"
+            host     = self.public_ip_address
+            private_key = tls_private_key.ssh_key.private_key_pem
+        }
+    } 
+
     provisioner "local-exec" {
         command = <<EOF
             ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook \
@@ -99,6 +108,15 @@ resource "paperspace_machine" "gradient_workers_cpu" {
     network_id = paperspace_network.network.handle
     live_forever = true
 
+    provisioner "remote-exec" {
+        connection {
+            type     = "ssh"
+            user     = "paperspace"
+            host     = self.public_ip_address
+            private_key = tls_private_key.ssh_key.private_key_pem
+        }
+    } 
+
     provisioner "local-exec" {
         command = <<EOF
             ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook \
@@ -129,6 +147,15 @@ resource "paperspace_machine" "gradient_workers_gpu" {
     script_id = paperspace_script.add_public_ssh_key.id
     network_id = paperspace_network.network.handle
     live_forever = true
+
+    provisioner "remote-exec" {
+        connection {
+            type     = "ssh"
+            user     = "paperspace"
+            host     = self.public_ip_address
+            private_key = tls_private_key.ssh_key.private_key_pem
+        }
+    } 
 
     provisioner "local-exec" {
         command = <<EOF
