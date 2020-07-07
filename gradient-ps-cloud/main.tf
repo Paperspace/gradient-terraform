ef3830ce5cb2264e3d9448d2b0861451306acd73
Jared Scheib <jared.scheib@gmail.com>
2020-07-07 18:23:59 -0400

    Revert "fix: force P4000 instead of cpu nodes for demo"
    This reverts commit f2fc406339beb7584a8c2a54b1955d95b97b86c1.

--- gradient-ps-cloud/main.tf
+++ gradient-ps-cloud/main.tf
@@ -45,7 +45,7 @@ resource "paperspace_machine" "gradient_main" {
 
     region = var.region
     name = "${var.cluster_handle}-${var.name}-main"
-    machine_type = "P4000"
+    machine_type = var.machine_type_main
     size = var.machine_storage_main
     billing_type = "hourly"
     assign_public_ip = true
@@ -87,7 +87,7 @@ resource "paperspace_machine" "gradient_workers_cpu" {
     count = var.machine_count_worker_cpu
     region = var.region
     name = "${var.cluster_handle}-${var.name}-worker-cpu-${count.index}"
-    machine_type = "P4000"
+    machine_type = var.machine_type_worker_cpu
     size = var.machine_storage_worker_cpu
     billing_type = "hourly"
     assign_public_ip = true
