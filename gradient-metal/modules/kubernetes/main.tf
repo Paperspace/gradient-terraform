1d9f1e4c3ec3ed62b913880003b62578c2d81c23
Philip Reichenberger <philip@paperspace.com>
2020-08-24 16:53:50 -0400

    Add tf-13 support

--- gradient-metal/modules/kubernetes/main.tf
+++ gradient-metal/modules/kubernetes/main.tf
@@ -1,3 +1,12 @@
+terraform {
+    required_providers {
+        rke = {
+            source = "rancher/rke"
+            version = "1.0.1"
+        }
+    }
+}
+
 locals {
     cluster_file = "cluster.yml"
 
