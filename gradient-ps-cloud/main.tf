59b5bce9bfb246fe5a11c9d19ff6546e557f9a76
Ben Batha <ben.batha@paperspace.com>
2021-04-13 18:33:17 -0400

    Revert "Revert "fix(ps-cloud): add reservations for kubelet to improve node stability CLD-308 (#139)" (#141)" (#143)
    This reverts commit bf5dbb8081641eebfb1d10cd7b3327c068c074eb.

--- gradient-ps-cloud/main.tf
+++ gradient-ps-cloud/main.tf
@@ -312,6 +312,16 @@ resource "rancher2_cluster" "main" {
             max_unavailable_controlplane = "1"
             max_unavailable_worker       = "10%"
         }
+
+        services {
+            kubelet {
+                extra_args = {
+                    "system-reserved" = "cpu=500m,memory=256Mi,ephemeral-storage=5Gi"
+                    "kube-reserved-cgroup" = "/podruntime.slice"
+                    "kube-reserved" = "cpu=500m,memory=256Mi,ephemeral-storage=5Gi"
+                }
+            }
+        }
   }
 }
 
