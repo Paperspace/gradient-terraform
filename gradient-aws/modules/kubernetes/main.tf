80c505413b737fe22c52ea8f7ab060148e298647
Ben Batha <ben.batha@paperspace.com>
2021-04-30 15:34:40 -0400

    fixup! fixup! fixup! fixup! fixup! feat(gradient-processing): optional workload image caching PLA-34

--- gradient-aws/modules/kubernetes/main.tf
+++ gradient-aws/modules/kubernetes/main.tf
@@ -336,7 +336,7 @@ locals {
             concat([
                 "paperspace.com/pool-name=${node_type}",
                 "paperspace.com/pool-type=${local.node_pool_types[node_type]}",
-                "paperspace.com/gradient-worker=${tostring(regex("^services", node_type))}",
+                "paperspace.com/gradient-worker=${tostring(length(regexall("^services", node_type)) > 0)}",
             ], local.kubelet_extra_args[node_type])
         )}"
 
